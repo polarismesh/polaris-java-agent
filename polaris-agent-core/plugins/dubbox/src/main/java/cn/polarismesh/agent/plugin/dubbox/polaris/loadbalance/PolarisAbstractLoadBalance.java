a3aed436750230907ac37e65ec226f8fa3e499fb
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2022-02-15 11:37:34 +0800

    dubbox plugin

---
+++ polaris-agent-core/plugins/dubbox/src/main/java/cn/polarismesh/agent/plugin/dubbox/polaris/loadbalance/PolarisAbstractLoadBalance.java
@@ -0,0 +1,10 @@
+package cn.polarismesh.agent.plugin.dubbox.polaris.loadbalance;
+
+import com.alibaba.dubbo.rpc.cluster.loadbalance.AbstractLoadBalance;
+
+/**
+ * 继承Dubbo的AbstractLoadBalance类，用于实现自己的LoadBalance策略
+ */
+public abstract class PolarisAbstractLoadBalance extends AbstractLoadBalance {
+
+}
