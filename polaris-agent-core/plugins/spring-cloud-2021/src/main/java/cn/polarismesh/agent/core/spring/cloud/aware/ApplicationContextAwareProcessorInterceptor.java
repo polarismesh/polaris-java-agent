c28880cc8b7776336fb89365ab15d3be7d032bd6
chuntaojun <liaochuntao@live.com>
2022-11-01 23:45:31 +0800

    fix:修复ApplicationContext持有失败

---
+++ polaris-agent-core/plugins/spring-cloud-2021/src/main/java/cn/polarismesh/agent/core/spring/cloud/aware/ApplicationContextAwareProcessorInterceptor.java
@@ -0,0 +1,44 @@
+/*
+ * Tencent is pleased to support the open source community by making Polaris available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package cn.polarismesh.agent.core.spring.cloud.aware;
+
+import cn.polarismesh.common.interceptor.AbstractInterceptor;
+import com.tencent.cloud.common.util.ApplicationContextAwareUtils;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ApplicationContextAwareProcessor;
+
+/**
+ * {@link  ApplicationContextAwareProcessor#ApplicationContextAwareProcessor(ConfigurableApplicationContext)}
+ *
+ * @author <a href="mailto:liaochuntao@live.com">liaochuntao</a>
+ */
+public class ApplicationContextAwareProcessorInterceptor implements AbstractInterceptor {
+
+	@Override
+	public void before(Object target, Object[] args) {
+
+	}
+
+	@Override
+	public void after(Object target, Object[] args, Object result, Throwable throwable) {
+		ConfigurableApplicationContext context = (ConfigurableApplicationContext) args[0];
+		ApplicationContextAwareUtils utils = new ApplicationContextAwareUtils();
+		utils.setApplicationContext(context);
+	}
+}
