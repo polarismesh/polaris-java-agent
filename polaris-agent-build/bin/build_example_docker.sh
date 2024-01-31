f00c030113c546beb668a38ca4a63983b0a8f7c5
andrew shan <45474304+andrewshan@users.noreply.github.com>
2024-01-31 16:09:37 +0800

    add Dockerfile for examples

---
+++ polaris-agent-build/bin/build_example_docker.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+# workdir root
+
+
+version=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='properties']/*[local-name()='revision']/text()" ../../pom.xml)
+echo "${version}" > version.txt
+
+cd ../..
+workdir=$(pwd)
+echo "workdir is ${workdir}"
+
+docker_tag=$1
+
+docker_repository="${DOCKER_REPOSITORY}"
+if [[ "${docker_repository}" == "" ]]; then
+    docker_repository="polarismesh"
+fi
+
+arch_list=( "amd64" "arm64" )
+# arch_list=( "amd64" )
+platforms=""
+
+for arch in ${arch_list[@]}; do
+    platforms+="linux/${arch},"
+done
+
+echo "${platforms}"
+platforms=${platforms%?}
+
+#this script must be run after build.sh
+pushd "polaris-agent-examples"
+docker_files=$(find ./ | grep -w "Dockerfile")
+for docker_file in ${docker_files}; do
+  folder_name=${docker_file%/*}
+  pushd "${folder_name}/"
+  repo_name=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='artifactId']/text()" ${folder_name}/pom.xml)
+
+  docker_tag="${version}-java8"
+  echo "docker repository java8: ${docker_repository}/${repo_name}, tag : ${docker_tag}"
+  docker buildx build -f ./Dockerfile -t ${docker_repository}/${repo_name}:${docker_tag}  --build-arg version=${version} --build-arg java_version=8 --platform ${platforms} --push ./
+
+  docker_tag="${version}-java11"
+  echo "docker repository java11: ${docker_repository}/${repo_name}, tag : ${docker_tag}"
+  docker buildx build -f ./Dockerfile -t ${docker_repository}/${repo_name}:${docker_tag}  --build-arg version=${version} --build-arg java_version=11 --platform ${platforms} --push ./
+
+  docker_tag="${version}-java17"
+  echo "docker repository java17: ${docker_repository}/${repo_name}, tag : ${docker_tag}"
+  docker buildx build -f ./Dockerfile -t ${docker_repository}/${repo_name}:${docker_tag}  --build-arg version=${version} --build-arg java_version=17 --platform ${platforms} --push ./
+  popd 
+done
+popd
