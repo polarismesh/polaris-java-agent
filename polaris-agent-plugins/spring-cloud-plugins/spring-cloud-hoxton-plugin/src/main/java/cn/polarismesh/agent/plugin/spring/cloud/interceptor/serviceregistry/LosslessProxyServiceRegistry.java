e72b3e7cbaf97db2e62d7ce435afdba2c1548cd6
shedfreewu <shedfreewu@tencent.com>
2024-03-09 16:34:27 +0800

    feat: support hoxton lossless

---
+++ polaris-agent-plugins/spring-cloud-plugins/spring-cloud-hoxton-plugin/src/main/java/cn/polarismesh/agent/plugin/spring/cloud/interceptor/serviceregistry/LosslessProxyServiceRegistry.java
@@ -0,0 +1,75 @@
+/*
+ * Tencent is pleased to support the open source community by making Polaris available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package cn.polarismesh.agent.plugin.spring.cloud.interceptor.serviceregistry;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import com.tencent.cloud.plugin.lossless.SpringCloudLosslessActionProvider;
+import com.tencent.cloud.polaris.context.PolarisSDKContextManager;
+
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.cloud.client.serviceregistry.ServiceRegistry;
+
+/**
+ * Lossless proxy for {@link ServiceRegistry}.
+ *
+ * @author Shedfree Wu
+ */
+public class LosslessProxyServiceRegistry implements ServiceRegistry<Registration> {
+
+	private final ServiceRegistry<Registration> target;
+
+	private PolarisSDKContextManager polarisSDKContextManager;
+
+	private final AtomicBoolean doneDeregister = new AtomicBoolean(false);
+
+	public LosslessProxyServiceRegistry(ServiceRegistry<Registration> target,
+			PolarisSDKContextManager polarisSDKContextManager, Registration registration) {
+		this.target = target;
+		this.polarisSDKContextManager = polarisSDKContextManager;
+	}
+
+	@Override
+	public void register(Registration registration) {
+		// port same with RegistryInterceptor#onBefore
+		polarisSDKContextManager.getLosslessAPI().losslessRegister(
+				SpringCloudLosslessActionProvider.getBaseInstance(registration, 0));
+	}
+
+	@Override
+	public void deregister(Registration registration) {
+		if (doneDeregister.compareAndSet(false, true)) {
+			target.deregister(registration);
+		}
+	}
+
+	@Override
+	public void close() {
+		target.close();
+	}
+
+	@Override
+	public void setStatus(Registration registration, String status) {
+		target.setStatus(registration, status);
+	}
+
+	@Override
+	public <T> T getStatus(Registration registration) {
+		return target.getStatus(registration);
+	}
+}
---
+++ polaris-agent-plugins/spring-cloud-plugins/spring-cloud-hoxton-plugin/src/main/java/cn/polarismesh/agent/plugin/spring/cloud/interceptor/serviceregistry/RegistryInterceptor.java
@@ -0,0 +1,78 @@
+/*
+ * Tencent is pleased to support the open source community by making Polaris available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package cn.polarismesh.agent.plugin.spring.cloud.interceptor.serviceregistry;
+
+import cn.polarismesh.agent.core.common.utils.ReflectionUtils;
+import cn.polarismesh.agent.plugin.spring.cloud.common.Holder;
+import cn.polarismesh.agent.plugin.spring.cloud.interceptor.BaseInterceptor;
+import com.tencent.cloud.common.util.ApplicationContextAwareUtils;
+import com.tencent.cloud.plugin.lossless.SpringCloudLosslessActionProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.client.serviceregistry.AbstractAutoServiceRegistration;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.cloud.client.serviceregistry.ServiceRegistry;
+
+/**
+ * hack {@link AbstractAutoServiceRegistration#start()}
+ * <p>
+ * Polaris Ribbon Server 实现类
+ */
+public class RegistryInterceptor extends BaseInterceptor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegistryInterceptor.class);
+
+    public RegistryInterceptor() {
+
+    }
+
+    @Override
+    public void onBefore(Object target, Object[] args) {
+        if (!Holder.isAllowDiscovery()) {
+            return;
+        }
+
+        Registration registration = ApplicationContextAwareUtils.getApplicationContext().getBean(Registration.class);
+
+        LOGGER.debug("[PolarisAgent] replace ServiceRegistry to ProxyServiceRegistry, target : {}", target);
+        ServiceRegistry<Registration> registry;
+        LosslessProxyServiceRegistry losslessProxyServiceRegistry;
+
+        String clsName = target.getClass().getCanonicalName();
+
+        if (clsName.contains("org.springframework.cloud.client.serviceregistry.AbstractAutoServiceRegistration")) {
+            registry = (ServiceRegistry<Registration>) ReflectionUtils.getObjectByFieldName(target, "serviceRegistry");
+            losslessProxyServiceRegistry = new LosslessProxyServiceRegistry(registry, Holder.getContextManager(), registration);
+            ReflectionUtils.setValueByFieldName(target, "serviceRegistry", losslessProxyServiceRegistry);
+        } else {
+            registry = (ServiceRegistry<Registration>) ReflectionUtils.getSuperObjectByFieldName(target, "serviceRegistry");
+            losslessProxyServiceRegistry = new LosslessProxyServiceRegistry(registry, Holder.getContextManager(), registration);
+            ReflectionUtils.setSuperValueByFieldName(target, "serviceRegistry", losslessProxyServiceRegistry);
+        }
+        Runnable originalRegisterAction = () -> registry.register(registration);
+
+        SpringCloudLosslessActionProvider losslessActionProvider = new SpringCloudLosslessActionProvider(
+            losslessProxyServiceRegistry, registration, Holder.getLosslessProperties(), originalRegisterAction);
+
+        // when server.port is 0, port not init
+        Holder.getContextManager().getLosslessAPI().setLosslessActionProvider(
+            SpringCloudLosslessActionProvider.getBaseInstance(registration, 0), losslessActionProvider);
+
+        LOGGER.debug("[PolarisAgent] finished replace ServiceRegistry to ProxyServiceRegistry");
+    }
+}
