ccc3e8d084fd4936ea9d7a0677eea9bdb7f5feaa
andrew shan <45474304+andrewshan@users.noreply.github.com>
2024-04-10 11:45:57 +0800

    feat: 支持spring-cloud-2023 examples的镜像打包 (#143)
    * feat: new add spring-cloud-2023 plugin
    
    * feat: fix rpc enhancement的bean重复加载的问题
    
    * feat: add deployment and dockerfiles for examples
    
    * feat: 解决开启路由后，服务调用报错的问题
    
    * feat: modify build docker scripts
    
    * feat: 更新example镜像文件

--- polaris-agent-build/bin/build_example_docker.sh
+++ polaris-agent-build/bin/build_example_docker.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+
+if [ $# -gt 0 ]; then
+  subdir="$1"
+fi
+
 # workdir root
-
-
 version=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='properties']/*[local-name()='revision']/text()" ../../pom.xml)
 echo "${version}" > version.txt
 
@@ -34,37 +37,47 @@ platforms=${platforms%?}
 pushd "polaris-agent-examples"
 docker_files=$(find ./ | grep -w "Dockerfile")
 for docker_file in ${docker_files}; do
-  folder_name=${docker_file%/*}
-  pushd "${folder_name}/"
-  repo_name=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='artifactId']/text()" pom.xml)
+  if [ -z $subdir ] || [[ ${docker_file} = "./${subdir}"* ]]; then
+    folder_name=${docker_file%/*}
+    pushd "${folder_name}/"
+    repo_name=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='artifactId']/text()" pom.xml)
+ 
+    echo "current is $(pwd)"
 
-  echo "current is $(pwd)"
-
-  dir_name=${folder_name##*/} 
+    dir_name=${folder_name##*/} 
   
   cp Dockerfile Dockerfile-${dir_name}
   docker_tag="${version}-java8"
 
-  filename=$(find ./target -maxdepth 1 -name "*.jar" | head -n 1)
+  filename=$(find ./target -maxdepth 1 -name "*.jar" | grep -v "javadoc" | grep -v "sources" | head -n 1)
   if [ -z "${filename}" ]; then
     echo "jar file not found"
     exit 1
   fi
   echo "jar file path is ${filename}"
 
-  echo "docker repository java8: ${docker_repository}/${repo_name}, tag : ${docker_tag}"
-  docker buildx build -f Dockerfile-${dir_name} --no-cache -t ${docker_repository}/${repo_name}:${docker_tag}  --build-arg file_name=${filename} --build-arg java_version=8 --platform ${platforms} --push ./
+  filename=${filename##*/}
+  echo "jar sub file path is ${filename}"
+  if [ `grep -c "java_version" Dockerfile` -gt 0 ]
+  then
+    echo "docker repository java8: ${docker_repository}/${repo_name}, tag : ${docker_tag}"
+    docker buildx build -f Dockerfile-${dir_name} --no-cache -t ${docker_repository}/${repo_name}:${docker_tag}  --build-arg file_name=${filename} --build-arg java_version=8 --platform ${platforms} --push ./
 
-  docker_tag="${version}-java11"
-  echo "docker repository java11: ${docker_repository}/${repo_name}, tag : ${docker_tag}"
-  docker buildx build -f Dockerfile-${dir_name} --no-cache -t ${docker_repository}/${repo_name}:${docker_tag}  --build-arg file_name=${filename} --build-arg java_version=11 --platform ${platforms} --push ./
+    docker_tag="${version}-java11"
+    echo "docker repository java11: ${docker_repository}/${repo_name}, tag : ${docker_tag}"
+    docker buildx build -f Dockerfile-${dir_name} --no-cache -t ${docker_repository}/${repo_name}:${docker_tag}  --build-arg file_name=${filename} --build-arg java_version=11 --platform ${platforms} --push ./
 
-  docker_tag="${version}-java17"
-  echo "docker repository java17: ${docker_repository}/${repo_name}, tag : ${docker_tag}"
-  docker buildx build -f Dockerfile-${dir_name} --no-cache -t ${docker_repository}/${repo_name}:${docker_tag}  --build-arg file_name=${filename} --build-arg java_version=17 --platform ${platforms} --push ./
+    docker_tag="${version}-java17"
+    echo "docker repository java17: ${docker_repository}/${repo_name}, tag : ${docker_tag}"
+    docker buildx build -f Dockerfile-${dir_name} --no-cache -t ${docker_repository}/${repo_name}:${docker_tag}  --build-arg file_name=${filename} --build-arg java_version=17 --platform ${platforms} --push ./
+  else
+    docker_tag="${version}-java17"
+    echo "docker repository java17: ${docker_repository}/${repo_name}, tag : ${docker_tag}"
+    docker buildx build -f Dockerfile-${dir_name} --no-cache -t ${docker_repository}/${repo_name}:${docker_tag}  --build-arg file_name=${filename} --platform ${platforms} --push ./
+  fi
 
-  rm Dockerfile-${dir_name}
-
+  rm -f Dockerfile-${dir_name}
   popd 
+  fi
 done
 popd
