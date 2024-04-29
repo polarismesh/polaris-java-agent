8f1330648e6b97b5a245c125c82c4b8dd6f8f07a
fredrikliu <139424001+fredrikliu@users.noreply.github.com>
2024-04-29 12:01:05 +0800

    add-java-agent-hoxton (#156)
    Co-authored-by: fredrikliu <fredrikliu@tencent.com>

--- polaris-agent-plugins/spring-cloud-plugins/spring-cloud-hoxton-plugin/src/main/java/cn/polarismesh/agent/plugin/spring/cloud/common/ClassNames.java
+++ polaris-agent-plugins/spring-cloud-plugins/spring-cloud-hoxton-plugin/src/main/java/cn/polarismesh/agent/plugin/spring/cloud/common/ClassNames.java
@@ -17,10 +17,17 @@
 
 package cn.polarismesh.agent.plugin.spring.cloud.common;
 
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.List;
 
+/**
+ * @author <a href="mailto:liaochuntao@live.com">liaochuntao</a>
+ */
 public class ClassNames {
+
 
 	/**
 	 * {@link org.springframework.cloud.client.serviceregistry.AbstractAutoServiceRegistration#start()}
@@ -28,7 +35,57 @@ public class ClassNames {
 	public static final String SERVICE_REGISTRATION = "org.springframework.cloud.client.serviceregistry.AbstractAutoServiceRegistration";
 
 	/**
+	 * {@link org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClient#CompositeDiscoveryClient(List)}
+	 */
+	public static final String DISCOVERY_CLIENT = "org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClient";
+
+	/**
+	 * {@link org.springframework.cloud.client.discovery.composite.reactive.ReactiveCompositeDiscoveryClient#ReactiveCompositeDiscoveryClient(List)}
+	 */
+	public static final String REACTIVE_DISCOVERY_CLIENT = "org.springframework.cloud.client.discovery.composite.reactive.ReactiveCompositeDiscoveryClient";
+
+	/**
+	 * {@link org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplierBuilder#withDiscoveryClient()}
+	 * {@link org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplierBuilder#withBlockingDiscoveryClient()}
+	 * {@link org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplierBuilder#withCaching()}
+	 */
+	public static final String ROUTER = "org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplierBuilder";
+
+	/**
+	 * {@link org.springframework.web.server.handler.FilteringWebHandler#FilteringWebHandler(org.springframework.web.server.WebHandler, List)}
+	 */
+	public static final String REACTIVE_WEB_FILTER = "org.springframework.web.server.handler.FilteringWebHandler";
+
+	/**
+	 * {@link org.springframework.web.servlet.DispatcherServlet#initStrategies(ApplicationContext)}
+	 */
+	public static final String SERVLET_WEB_FILTER = "org.springframework.web.servlet.DispatcherServlet";
+
+	/**
+	 * {@link org.springframework.http.client.support.InterceptingHttpAccessor#getInterceptors()}
+	 */
+	public static final String REST_TEMPLATE = "org.springframework.http.client.support.InterceptingHttpAccessor";
+
+	/**
+	 * {@link org.springframework.cloud.openfeign.FeignClientFactoryBean#getInheritedAwareInstances(FeignContext, Class)}
+	 */
+	public static final String FEIGN_TEMPLATE = "org.springframework.cloud.openfeign.FeignClientFactoryBean";
+
+	/**
 	 * {@link org.springframework.context.support.ApplicationContextAwareProcessor#ApplicationContextAwareProcessor(ConfigurableApplicationContext)}
 	 */
 	public static final String APPLICATION_CONTEXT_AWARE = "org.springframework.context.support.ApplicationContextAwareProcessor";
+
+	/**
+	 * {@link org.springframework.boot.env.EnvironmentPostProcessorApplicationListener#onApplicationEnvironmentPreparedEvent(ApplicationEnvironmentPreparedEvent)}
+	 */
+	public static final String ENVIRONMENT_POST_PROCESSOR = "org.springframework.boot.env.EnvironmentPostProcessorApplicationListener";
+
+	/**
+	 * {@link org.springframework.cloud.loadbalancer.blocking.client.BlockingLoadBalancerClient}
+	 */
+	public static final String BLOCKING_LOADBALANCER_CLIENT = "org.springframework.cloud.loadbalancer.blocking.client.BlockingLoadBalancerClient";
+
+
+	public static final String INTERCEPTING_HTTP_ACCESSOR  = "org.springframework.http.client.support.InterceptingHttpAccessor";
 }
