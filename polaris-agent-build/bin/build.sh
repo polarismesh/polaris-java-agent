42e433f8abacd64c40367e48cfd0ee37a177c4ef
Haotian Zhang <928016560@qq.com>
2024-06-20 20:52:28 +0800

    feat:update build.sh.

--- polaris-agent-build/bin/build.sh
+++ polaris-agent-build/bin/build.sh
@@ -39,12 +39,19 @@ cp "polaris-agent-core/polaris-agent-core-optional-java17/target/polaris-agent-c
 cp "polaris-agent-core/polaris-agent-core-extension/target/polaris-agent-core-extension-${version}.jar" "${folder_name}/boot/"
 
 pushd "polaris-agent-plugins"
-plugin_folders=$(find . -maxdepth 2 | grep -E ".+-plugin-common$")
+plugin_folders=$(find . -maxdepth 2 | grep -E ".+-plugin$")
 for plugin_folder in ${plugin_folders}; do
   file_name=${plugin_folder##*/}
   cp "${plugin_folder}/target/${file_name}-${version}.jar" "../${folder_name}/plugins/"
   if [ -d "${plugin_folder}/src/main/conf" ]; then
     cp -r "${plugin_folder}/src/main/conf/." "../${folder_name}/conf/"
+  fi
+done
+plugin_config_folders=$(find . -maxdepth 2 | grep -E ".+-plugin-common$")
+for plugin_config_folder in ${plugin_config_folders}; do
+  file_name=${plugin_config_folder##*/}
+  if [ -d "${plugin_config_folder}/src/main/conf" ]; then
+    cp -r "${plugin_config_folder}/src/main/conf/." "../${folder_name}/conf/"
   fi
 done
 popd
