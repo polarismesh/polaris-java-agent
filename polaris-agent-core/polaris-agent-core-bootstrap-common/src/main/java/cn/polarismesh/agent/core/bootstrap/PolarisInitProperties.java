e8d21e8e3c60e3d9aa398fa651f5ac59f5f5c300
fredrikliu <fredrikliu@tencent.com>
2024-06-24 11:18:37 +0800

    add annotation

--- polaris-agent-core/polaris-agent-core-bootstrap-common/src/main/java/cn/polarismesh/agent/core/bootstrap/PolarisInitProperties.java
+++ polaris-agent-core/polaris-agent-core-bootstrap-common/src/main/java/cn/polarismesh/agent/core/bootstrap/PolarisInitProperties.java
@@ -4,6 +4,7 @@ public class PolarisInitProperties {
 
     public void initialize() {
 
+        // ban nacos as sct currently does not support double registration
         System.setProperty("spring.cloud.nacos.config.enabled", "false");
         System.setProperty("spring.cloud.nacos.discovery.enabled", "false");
 
