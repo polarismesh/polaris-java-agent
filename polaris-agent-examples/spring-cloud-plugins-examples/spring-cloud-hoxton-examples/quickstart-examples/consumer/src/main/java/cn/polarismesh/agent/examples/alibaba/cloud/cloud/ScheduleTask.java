1ddff07d10872ed4352e51c7bfb8e4a3c3e42e9f
evelynwei <evelynwei@tencent.com>
2025-05-06 19:20:09 +0800

    feat[hoxton]: upgrade and support config

---
+++ polaris-agent-examples/spring-cloud-plugins-examples/spring-cloud-hoxton-examples/quickstart-examples/consumer/src/main/java/cn/polarismesh/agent/examples/alibaba/cloud/cloud/ScheduleTask.java
@@ -0,0 +1,53 @@
+package cn.polarismesh.agent.examples.alibaba.cloud.cloud;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@EnableScheduling
+@Service
+public class ScheduleTask {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ScheduleTask.class);
+
+    @Autowired
+    @Qualifier("restTemplate")
+    private RestTemplate template;
+
+    @Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
+
+    @Value("${consumer.auto.test:true}")
+    private Boolean autoTest;
+
+    @Scheduled(fixedDelayString = "${consumer.auto.test.interval:30000}")
+    public void autoSendRequest() {
+        if (!autoTest) {
+            LOG.info("自动请求关闭");
+            return;
+        }
+        String[] testStrings = {"test1", "test2", "auto-test"};
+        for (String str : testStrings) {
+            try {
+                ResponseEntity<String> response = circuitBreakerFactory.create("autoRequestCircuitBreaker")
+                        .run(() -> template.getForEntity("http://service-provider-hoxton/echo/" + str, String.class),
+                                throwable -> {
+                                    LOG.error("自动请求失败: {}", throwable.getMessage());
+                                    return ResponseEntity.status(503).body("服务暂不可用");
+                                });
+                LOG.info("自动请求[{}]响应: {}", str, response.getBody());
+            } catch (Exception e) {
+                LOG.error("自动请求异常: {}", e.getMessage());
+            }
+        }
+    }
+
+}
--- polaris-agent-plugins/spring-cloud-plugins/spring-cloud-hoxton-plugin/src/main/java/cn/polarismesh/agent/plugin/spring/cloud/inject/RouterBeanInjector.java
+++ polaris-agent-plugins/spring-cloud-plugins/spring-cloud-hoxton-plugin/src/main/java/cn/polarismesh/agent/plugin/spring/cloud/inject/RouterBeanInjector.java
@@ -17,10 +17,6 @@
 
 package cn.polarismesh.agent.plugin.spring.cloud.inject;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-import java.util.Map;
-
 import cn.polarismesh.agent.core.common.utils.ClassUtils;
 import cn.polarismesh.agent.core.common.utils.ReflectionUtils;
 import cn.polarismesh.agent.plugin.spring.cloud.common.BeanInjector;
@@ -28,10 +24,17 @@ import cn.polarismesh.agent.plugin.spring.cloud.common.Constant;
 import cn.polarismesh.agent.plugin.spring.cloud.common.Utils;
 import com.tencent.cloud.polaris.router.config.FeignAutoConfiguration;
 import com.tencent.cloud.polaris.router.config.RouterAutoConfiguration;
+import com.tencent.cloud.polaris.router.config.RouterConfigModifierAutoConfiguration;
+import com.tencent.cloud.polaris.router.config.properties.PolarisMetadataRouterProperties;
+import com.tencent.cloud.polaris.router.config.properties.PolarisNamespaceRouterProperties;
+import com.tencent.cloud.polaris.router.config.properties.PolarisNearByRouterProperties;
+import com.tencent.cloud.polaris.router.config.properties.PolarisRuleBasedRouterProperties;
 import com.tencent.cloud.polaris.router.endpoint.PolarisRouterEndpointAutoConfiguration;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.core.env.Environment;
@@ -67,6 +70,38 @@ public class RouterBeanInjector implements BeanInjector {
 		ReflectionUtils.invokeMethod(processConfigurationClass, configurationParser, polarisRouterEndpointAutoConfiguration, Constant.DEFAULT_EXCLUSION_FILTER);
 		registry.registerBeanDefinition("polarisRouterEndpointAutoConfiguration", BeanDefinitionBuilder.genericBeanDefinition(
 				PolarisRouterEndpointAutoConfiguration.class).getBeanDefinition());
+
+		Object polarisNearByRouterProperties = ReflectionUtils.invokeConstructor(configClassCreator,
+				PolarisNearByRouterProperties.class, "polarisNearByRouterProperties");
+		ReflectionUtils.invokeMethod(processConfigurationClass, configurationParser, polarisNearByRouterProperties,
+				Constant.DEFAULT_EXCLUSION_FILTER);
+		registry.registerBeanDefinition("polarisNearByRouterProperties", BeanDefinitionBuilder.genericBeanDefinition(
+				PolarisNearByRouterProperties.class).getBeanDefinition());
+		Object polarisRuleBasedRouterProperties = ReflectionUtils.invokeConstructor(configClassCreator,
+				PolarisRuleBasedRouterProperties.class, "polarisRuleBasedRouterProperties");
+		ReflectionUtils.invokeMethod(processConfigurationClass, configurationParser, polarisRuleBasedRouterProperties,
+				Constant.DEFAULT_EXCLUSION_FILTER);
+		registry.registerBeanDefinition("polarisRuleBasedRouterProperties", BeanDefinitionBuilder.genericBeanDefinition(
+				PolarisRuleBasedRouterProperties.class).getBeanDefinition());
+		Object polarisNamespaceRouterProperties = ReflectionUtils.invokeConstructor(configClassCreator,
+				PolarisNamespaceRouterProperties.class, "polarisNamespaceRouterProperties");
+		ReflectionUtils.invokeMethod(processConfigurationClass, configurationParser, polarisNamespaceRouterProperties,
+				Constant.DEFAULT_EXCLUSION_FILTER);
+		registry.registerBeanDefinition("polarisNamespaceRouterProperties", BeanDefinitionBuilder.genericBeanDefinition(
+				PolarisNamespaceRouterProperties.class).getBeanDefinition());
+		Object routerConfigModifierAutoConfiguration = ReflectionUtils.invokeConstructor(configClassCreator,
+				RouterConfigModifierAutoConfiguration.class, "routerConfigModifierAutoConfiguration");
+		ReflectionUtils.invokeMethod(processConfigurationClass, configurationParser,
+				routerConfigModifierAutoConfiguration, Constant.DEFAULT_EXCLUSION_FILTER);
+		registry.registerBeanDefinition("routerConfigModifierAutoConfiguration",
+				BeanDefinitionBuilder.genericBeanDefinition(
+						RouterConfigModifierAutoConfiguration.class).getBeanDefinition());
+		Object polarisMetadataRouterProperties = ReflectionUtils.invokeConstructor(configClassCreator,
+				PolarisMetadataRouterProperties.class, "polarisMetadataRouterProperties");
+		ReflectionUtils.invokeMethod(processConfigurationClass, configurationParser, polarisMetadataRouterProperties,
+				Constant.DEFAULT_EXCLUSION_FILTER);
+		registry.registerBeanDefinition("polarisMetadataRouterProperties", BeanDefinitionBuilder.genericBeanDefinition(
+				PolarisMetadataRouterProperties.class).getBeanDefinition());
 
 		// make FeignRibbonClientAutoConfiguration later
 		Map<Object, Object> configurationClasses =  (Map<Object, Object>) ReflectionUtils.getObjectByFieldName(configurationParser, "configurationClasses");
