3a677712519effa9b323f5b14904e0adf3183c4c
evelynwei <evelynwei@tencent.com>
2025-05-15 19:56:50 +0800

    fix: 2022 demo update

--- polaris-agent-examples/spring-cloud-plugins-examples/spring-cloud-2022-examples/quickstart-examples/consumer/src/main/java/cn/polarismesh/agent/examples/alibaba/cloud/cloud/ScheduleTask.java
+++ polaris-agent-examples/spring-cloud-plugins-examples/spring-cloud-2022-examples/quickstart-examples/consumer/src/main/java/cn/polarismesh/agent/examples/alibaba/cloud/cloud/ScheduleTask.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,7 +14,6 @@ import org.springframework.web.client.RestTemplate;
 
 @EnableScheduling
 @Service
-@RefreshScope
 public class ScheduleTask {
 
     private static final Logger LOG = LoggerFactory.getLogger(ScheduleTask.class);
@@ -35,21 +33,18 @@ public class ScheduleTask {
         if (!autoTest) {
             return;
         }
-
         String[] testStrings = {"test1", "test2", "auto-test"};
-        var circuitBreaker = circuitBreakerFactory.create("autoRequestCircuitBreaker");
-
         for (String str : testStrings) {
             try {
-                ResponseEntity<String> response = circuitBreaker.run(
-                        () -> template.getForEntity("http://service-provider-2022/echo/" + str, String.class),
-                        throwable -> {
-                            LOG.error("自动请求失败: {}", throwable.getMessage(), throwable);
-                            return ResponseEntity.status(503).body("服务暂不可用");
-                        });
+                ResponseEntity<String> response = circuitBreakerFactory.create("autoRequestCircuitBreaker")
+                        .run(() -> template.getForEntity("http://service-provider-2022/echo/" + str, String.class),
+                                throwable -> {
+                                    LOG.error("自动请求失败: {}", throwable.getMessage());
+                                    return ResponseEntity.status(503).body("服务暂不可用");
+                                });
                 LOG.info("自动请求[{}]响应: {}", str, response.getBody());
             } catch (Exception e) {
-                LOG.error("自动请求异常: {}", e.getMessage(), e);
+                LOG.error("自动请求异常: {}", e.getMessage());
             }
         }
     }
