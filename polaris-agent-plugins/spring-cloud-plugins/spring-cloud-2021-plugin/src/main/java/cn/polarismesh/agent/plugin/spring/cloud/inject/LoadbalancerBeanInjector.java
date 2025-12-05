4b1025bb16ed81caf5dbae5d1ce9518b04ce773d
fishtailfu <fishtailfu@tencent.com>
2025-12-05 17:44:50 +0800

    feat: support nacos&polaris registry and new lb strategies.

--- polaris-agent-plugins/spring-cloud-plugins/spring-cloud-2021-plugin/src/main/java/cn/polarismesh/agent/plugin/spring/cloud/inject/LoadbalancerBeanInjector.java
+++ polaris-agent-plugins/spring-cloud-plugins/spring-cloud-2021-plugin/src/main/java/cn/polarismesh/agent/plugin/spring/cloud/inject/LoadbalancerBeanInjector.java
@@ -22,6 +22,9 @@ import cn.polarismesh.agent.plugin.spring.cloud.common.BeanInjector;
 import cn.polarismesh.agent.plugin.spring.cloud.common.Constant;
 import cn.polarismesh.agent.plugin.spring.cloud.common.Utils;
 import com.tencent.cloud.polaris.loadbalancer.PolarisLoadBalancerAutoConfiguration;
+import com.tencent.cloud.polaris.loadbalancer.PolarisLoadBalancerBootstrapAutoConfiguration;
+import com.tencent.cloud.polaris.loadbalancer.PolarisLoadBalancerPropertiesAutoConfiguration;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
@@ -35,17 +38,38 @@ public class LoadbalancerBeanInjector implements BeanInjector {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoadbalancerBeanInjector.class);
 
+    private final AtomicBoolean bootstrapLoaded = new AtomicBoolean(false);
+
     @Override
     public String getModule() {
         return "spring-cloud-tencent-polaris-loadbalancer";
     }
-
+    @Override
+    public void onBootstrapStartup(Object configurationParser, Constructor<?> configClassCreator,
+            Method processConfigurationClass, BeanDefinitionRegistry registry, Environment environment) {
+        bootstrapLoaded.set(true);
+        Object polarisLoadBalancerBootstrapAutoConfiguration = ReflectionUtils.invokeConstructor(configClassCreator,
+                PolarisLoadBalancerBootstrapAutoConfiguration.class, "polarisLoadBalancerBootstrapAutoConfiguration");
+        ReflectionUtils.invokeMethod(processConfigurationClass, configurationParser,
+                polarisLoadBalancerBootstrapAutoConfiguration, Constant.DEFAULT_EXCLUSION_FILTER);
+        registry.registerBeanDefinition("polarisLoadBalancerBootstrapAutoConfiguration",
+                BeanDefinitionBuilder.genericBeanDefinition(PolarisLoadBalancerBootstrapAutoConfiguration.class)
+                        .getBeanDefinition());
+        LOGGER.info("[PolarisJavaAgent] success to inject bootstrap bean definitions for module {}", getModule());
+    }
     @Override
     public void onApplicationStartup(Object configurationParser, Constructor<?> configClassCreator, Method processConfigurationClass, BeanDefinitionRegistry registry, Environment environment) {
         if (!(Utils.checkPolarisEnabled(environment) && Utils.checkKeyEnabled(environment, "spring.cloud.polaris.loadbalancer.enabled"))) {
             LOGGER.warn("[PolarisJavaAgent] polaris loadbalancer not enabled, skip inject application bean definitions for module {}", getModule());
             return;
         }
+        if (!bootstrapLoaded.get()) {
+            onBootstrapStartup(configurationParser, configClassCreator, processConfigurationClass, registry, environment);
+        }
+        Object polarisLoadBalancerPropertiesAutoConfiguration = ReflectionUtils.invokeConstructor(configClassCreator, PolarisLoadBalancerPropertiesAutoConfiguration.class, "polarisLoadBalancerPropertiesAutoConfiguration");
+        ReflectionUtils.invokeMethod(processConfigurationClass, configurationParser, polarisLoadBalancerPropertiesAutoConfiguration, Constant.DEFAULT_EXCLUSION_FILTER);
+        registry.registerBeanDefinition("polarisLoadBalancerPropertiesAutoConfiguration", BeanDefinitionBuilder.genericBeanDefinition(
+                PolarisLoadBalancerPropertiesAutoConfiguration.class).getBeanDefinition());
         Object polarisLoadBalancerAutoConfiguration = ReflectionUtils.invokeConstructor(configClassCreator, PolarisLoadBalancerAutoConfiguration.class, "polarisLoadBalancerAutoConfiguration");
         ReflectionUtils.invokeMethod(processConfigurationClass, configurationParser, polarisLoadBalancerAutoConfiguration, Constant.DEFAULT_EXCLUSION_FILTER);
         registry.registerBeanDefinition("polarisLoadBalancerAutoConfiguration", BeanDefinitionBuilder.genericBeanDefinition(
