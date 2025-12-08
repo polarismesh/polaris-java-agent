efb198c122d59a2b672935217a2a186fb1d7d06f
fishtailfu <fishtailfu@tencent.com>
2025-12-08 15:10:53 +0800

    feat: support nacos&polaris registry and update lb strategies.

--- polaris-agent-core/polaris-agent-core-bootstrap-common/src/main/java/cn/polarismesh/agent/core/bootstrap/PolarisInitProperties.java
+++ polaris-agent-core/polaris-agent-core-bootstrap-common/src/main/java/cn/polarismesh/agent/core/bootstrap/PolarisInitProperties.java
@@ -20,9 +20,6 @@ package cn.polarismesh.agent.core.bootstrap;
 public class PolarisInitProperties {
 
     public void initialize() {
-
-        // ban nacos as sct currently does not support double registration
-//        System.setProperty("spring.cloud.nacos.discovery.enabled", "false");
         System.setProperty("spring.cloud.loadbalancer.nacos.enabled", "false");
     }
 }
